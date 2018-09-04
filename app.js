35d5a4e82d2c3e772c550e5c748b60c8716732a5
Carol junqueira <junqueiracarolina@live.com>
2018-09-04 15:39:28 -0300

    comentarios

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+// packages
 const express = require('express');
+// app instance, one application
 const app = express();
 
+// route "/"
 app.get('/', function(req,res){
+  // the response to client is "Maoee Silvio!"
   res.send("Maoee Silvio!");
 })
 
+// open connection on port 3000
 app.listen(3000,function(){
   console.log("Ouvindo a porta 3000!");
 })
