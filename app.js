7c3b50da0dc07a47ca5ae70f056301aebb91562b
Carol junqueira <junqueiracarolina@live.com>
2018-09-04 21:47:50 -0300

    correcoes post rotas

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/register', function(req,res){
   res.render('register');
 })
 
-app.post('/register', function(req,res){
+app.post('/post-register', function(req,res){
   res.render('map');
   console.log(req.body);
 })
