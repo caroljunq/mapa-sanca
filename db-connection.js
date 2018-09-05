e1520fad42c504c5b9038fb7df28a170b551077b
Carol junqueira <junqueiracarolina@live.com>
2018-09-05 16:50:13 -0300

    versao final

--- db-connection.js
+++ db-connection.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 // alternatives
-// mongodb://aluno:secomp123@ds233212.mlab.com:33212/mapa-sanca
 // mongodb://localhost:27017/mapa-sanca
-mongoose.connect("mongodb://138.197.170.34:8080/mapa-sanca", { useNewUrlParser: true },function(err){
+mongoose.connect("mongodb://aluno:secomp123@ds233212.mlab.com:33212/mapa-sanca", { useNewUrlParser: true },function(err){
   if(err){
     console.log(err);
   }
