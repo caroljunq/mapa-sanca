474b48b9f8114f01199ffafe3fa1b5fb492b219c
Carol junqueira <junqueiracarolina@live.com>
2018-09-05 16:46:31 -0300

    mais lindo ainda

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mapCtrl = require("./controllers/map-controller");
 
 app.set('view engine','ejs');
+app.use('/public', express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
